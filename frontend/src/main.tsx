2149cb2bcc077e3ce11cbb80a6dc04110d75caa5
YadurajManu <yadurajsingham@gmail.com>
2026-02-07 18:14:51 +0530

    Clean up project structure and leftover Next.js files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import './app/globals.css'
+import './index.css'
 import { AuthProvider } from './context/AuthContext'
 import { SocketProvider } from './context/SocketContext'
 
